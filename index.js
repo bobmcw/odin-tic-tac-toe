a4649e3a0ecd3bc286600d515150c569547e85e1
bob <bobmcwishon@gmail.com>
2024-05-25 14:50:33 +0200

    reformated code

--- index.js
+++ index.js
@@ -19,6 +19,15 @@ function GameBoard(){
    const inputMark = (row,col,player) => {
     board[row][col].setMark(player.getMark());
    }
+   const drawBoard = () => {
+   const values = board.map((row) => row.map((cell) => cell.getMark()));
+   for(let i=0;i<values.length();i++){
+    for(let j=0;j<values[i].length();j++){
+        let  
+    }
+   }
+
+   }
     return {printBoard,inputMark,getSpace}
 }
 function Cell(){
@@ -48,14 +57,14 @@ function GameController(){
     const checkForWin = () =>{
         //check for win verticaly
         const playerMark = activePlayer.getMark()
-        for(i=0;i<3;i++){
+        for(let i=0;i<3;i++){
             if (gameboard.getSpace(i,0) === playerMark && gameboard.getSpace(i,1) === playerMark && gameboard.getSpace(i,2) === playerMark){ 
                 console.log(`${gameboard.getSpace(i,0)} won!`);
                 return true;
             }
         }
         //check for win horizontaly
-        for(i=0;i<3;i++){
+        for(let i=0;i<3;i++){
             if (gameboard.getSpace(0,i) === playerMark && gameboard.getSpace(1,i) === playerMark && gameboard.getSpace(2,i) === playerMark){             
                 console.log(`${gameboard.getSpace(i,0)} won!`);
                 return true;
@@ -72,7 +81,7 @@ function GameController(){
             }
     }
     const turn = () => {
-        for(k=0;k<8;k++){
+        for(let k=0;k<8;k++){
         console.log(`${activePlayer.getMark()}'s turn`)
         placeMark(activePlayer)
         if(checkForWin()){
